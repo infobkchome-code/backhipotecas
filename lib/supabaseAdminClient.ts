e06fb1d3a357bfdd0e28f3855127002516a6f20e
infobkchome-code <infobkchome@gmail.com>
2025-11-14 10:17:39 +0100

    Update supabaseAdminClient.ts

--- lib/supabaseAdminClient.ts
+++ lib/supabaseAdminClient.ts
@@ -5,11 +5,15 @@ const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 // Seguridad: avisar si falta algo
 if (!supabaseUrl) {
-  throw new Error('Missing NEXT_PUBLIC_SUPABASE_URL in supabaseAdminClient.ts');
+  throw new Error(
+    'Missing NEXT_PUBLIC_SUPABASE_URL in supabaseAdminClient.ts'
+  );
 }
 
 if (!serviceRoleKey) {
-  throw new Error('Missing SUPABASE_SERVICE_ROLE_KEY in supabaseAdminClient.ts');
+  throw new Error(
+    'Missing SUPABASE_SERVICE_ROLE_KEY in supabaseAdminClient.ts'
+  );
 }
 
 // Cliente ADMIN (bypassea RLS)
