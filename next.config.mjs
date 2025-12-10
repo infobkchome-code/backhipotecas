aab48e457feef0f27f060ff1af9e9ff0247c6438
infobkchome-code <infobkchome@gmail.com>
2025-12-10 20:00:10 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  // output: "export", // ❌ QUITAR
+  // output: "standalone", // ✅ opcional
 };
-export default nextConfig;
+
+module.exports = nextConfig;
